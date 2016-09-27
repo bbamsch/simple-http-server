a8ba50f193667195a136ec0d33be0e7eae2c17b6
Brian Bamsch <bbamsch32@gmail.com>
2016-09-27 00:07:58 -0700

    Implement SocketHandlerTask functionality

--- src/main/java/com/bbamsch/simpleserver/task/SocketHandlerTask.java
+++ src/main/java/com/bbamsch/simpleserver/task/SocketHandlerTask.java
@@ -30,5 +30,38 @@ public class SocketHandlerTask implements Runnable {
 
     @Override
     public void run() {
+        try {
+            HttpRequest httpRequest = new HttpRequest(socket);
+            HttpResponse httpResponse = new HttpResponse(httpRequest);
+            OutputStream outputStream = socket.getOutputStream();
+
+            String webRoot = String.valueOf(server.getConfiguration().getOrDefault("content_root", "webroot/"));
+            String urlPath = httpRequest.getPath().substring(1);
+
+            File file = new File(webRoot, urlPath);
+            if (file.isDirectory()) {
+                file = new File(file, "index.html");
+            }
+            if (file.exists()) {
+                Path filePath = FileSystems.getDefault().getPath(file.getPath());
+                httpResponse.setFilePath(filePath);
+                httpResponse.setStatus(HttpStatus.OK);
+            } else {
+                httpResponse.setStatus(HttpStatus.NOT_FOUND);
+            }
+            outputStream.write(httpResponse.asBytes());
+
+            socket.close();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Exception while running SocketHandlerTask", e);
+        } finally {
+            try {
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                // Socket cleanup is best-effort
+            }
+        }
     }
 }
